efe4d3888e7eb5316768035f93dc3e0454dd45ed
Pavlo Getta <pavlo.getta@epfl.ch>
2018-07-30 11:54:02 +0200

    Rename component .props to .attrs

--- extensions/src/components/extensions/e-model/index.js
+++ extensions/src/components/extensions/e-model/index.js
@@ -3,7 +3,7 @@ import AnalysisComponent from '@/components/shared/analysis-component.vue';
 
 export default [{
   VueComponent: AnalysisComponent,
-  props: {
     name: 'E-Model Analysis',
+  attrs: {
   },
 }];
--- extensions/src/components/extensions/me-model/index.js
+++ extensions/src/components/extensions/me-model/index.js
@@ -3,7 +3,7 @@ import AnalysisComponent from '@/components/shared/analysis-component.vue';
 
 export default [{
   VueComponent: AnalysisComponent,
-  props: {
     name: 'ME-Model Analysis',
+  attrs: {
   },
 }];
